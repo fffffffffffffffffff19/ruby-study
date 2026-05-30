47be8f226e02f3151f6b0a9f0819e8b33ced1593
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-05-30 13:01:54 -0300

    feat(hello-world): add hello world and ways to print

---
+++ 01-basics/00_hello_world.rb
@@ -0,0 +1,18 @@
+# "Hello world ?"
+# learning how comments work "#"
+
+# printing things on console
+print "i love strawberrys\n"
+puts "i love strawberrys"
+p "i love strawberrys"
+
+# on variable
+greeting = "i love strawberrys!"
+puts greeting
+
+# on function with method
+def say_things(things)
+    puts things
+end
+
+say_things("i love strawberrys!!")
