ca5ff876526fd5d0796f7d664ece837e3cfbe3d9
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-05-31 10:58:51 -0300

    feat(numbers): add basic number lesson

---
+++ 01-basics/03_numbers.rb
@@ -0,0 +1,20 @@
+# Numbers
+puts 'Welcome to simple calculator'
+20.times {  print '-'  }
+puts
+
+#puts 'Enter Operand1:'
+#operand1 = gets.chomp
+operand1 = rand(101)
+puts "operand1 = #{operand1}"
+#puts 'Enter Operand2:'
+#operand2 = gets.chomp
+operand2 = rand(101)
+puts "operand2 = #{operand2}"
+
+puts
+puts "Operand1 + Operand2 = #{operand1.to_i + operand2.to_i}"
+puts "Operand1 - Operand2 = #{operand1.to_i - operand2.to_i}"
+puts "Operand1 * Operand2 = #{operand1.to_i * operand2.to_i}"
+puts "Operand1 / Operand2 = #{operand1.to_f / operand2.to_f}"
+puts "The remainder  after Operand1 / Operand2 = #{operand1.to_f % operand2.to_f}"
