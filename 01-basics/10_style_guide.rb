b6c422beeeebd33160f5420ec4bf9fc2f3f878de
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-06-03 09:25:41 -0300

    feat(style-guide) add basic style guide lesson

---
+++ 01-basics/10_style_guide.rb
@@ -0,0 +1,15 @@
+# Snake case
+this_is_snake_case = 999
+also_snake_case = "999"
+
+def my_name_is
+    # and my code is here
+end
+
+# CamelCase
+thisIsCamelCase = 999
+AlsoThisIsCamelCase = "999"
+
+class SomeClass
+    # any code here
+ end
